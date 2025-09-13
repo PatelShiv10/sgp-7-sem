e981860711b4ab8b751657f5372fce4c24bd2e57
Shiv Patel <patelshiv2123@gmail.com>
2025-09-13 18:05:57 +0530

    .

--- backend/routes/booking.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getExpandedSlots, createBooking } = require('../controllers/bookingController');
-const { protect } = require('../middlewares/authMiddleware');
-
-router.get('/lawyers/:lawyerId/slots', getExpandedSlots);
-router.post('/', protect, createBooking);
-
-module.exports = router;
